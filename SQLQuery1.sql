40dae03f73a5c5a30a5e97f8cf73104c5bb8aed7
Aksh Garg <104502015+akshgarg10@users.noreply.github.com>
2023-06-10 01:21:03 +0530

    Delete SQLQuery1.sql

--- SQLQuery1.sql
+++
@@ -1,8 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [user_id]
-      ,[booking_id]
-      ,[start_date]
-      ,[end_date]
-      ,[booking_time]
-      ,[booked]
-  FROM [Login_conference].[dbo].[booking]
